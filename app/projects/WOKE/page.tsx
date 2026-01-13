0e500a3373ec022b14e1207bce585dbcf73da666
groenbacke <frojdoadam@gmail.com>
2026-01-13 13:39:34 +0200

    erghrgsdrfgdrfg

--- app/projects/WOKE/page.tsx
+++ app/projects/WOKE/page.tsx
@@ -1,7 +1,13 @@
+"use client"
+import Navbar from '@/app/components/navbar'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+        <Navbar></Navbar>
+        <p>i have game you can play on itch.io (no downloadd)</p>
+        <a href="https://800-snails-the-stars-pi.itch.io/i-woke-up-to-this" id='loink'>if you want the game then heres the game here it is have the game here where you can play the game on itch io and itch io is the place where you can play my game my game is on itch io and here you play the game without download and you dont have to download the game but you can but you dont have to on itch io where you can download and play the game but you can also play on the website without having to download any game and then you play the game on itch io where games are played thank you</a>
+    </div>
   )
 }
--- app/projects/iminimini/page.tsx
+++ app/projects/iminimini/page.tsx
@@ -12,12 +12,18 @@ function page() {
 
         <div id="containgagmens">
             <div id="hope">
-                <Image alt="robbbob" width={500} height={500}  src="/art1.png"></Image>
+                <div className='bloit'>
+                    <Image alt="robbbob" width={500} height={500}  src="/arttttt/art1.png"></Image>
+                </div>
                 <p id="andadarkwindblows">background photo taken during a bus trip to Vaasa. The text is my full alias</p>
             </div>
             <div id="hope">
-                <Image alt="robbbob" width={500} height={500}  src="/art2.png"></Image>
+                <Image className='bloit' alt="robbbob" width={500} height={500}  src="/arttttt/art2.png"></Image>
                 <p id="andadarkwindblows">Godspeed You! Black Emperor is my favorite band</p>
+            </div>
+            <div id="hope">
+                <Image className='bloit' alt="robbbob" width={500} height={500}  src="/arttttt/art4.png"></Image>
+                <p id="andadarkwindblows">Not much changed from the original image. Im proud of how the jetfighters turned out</p>
             </div>
         </div>
     </div>
